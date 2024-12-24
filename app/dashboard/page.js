b68d7be8a82637da59a6f843b63d59bfab44fea5
Phan Minh Cuong <levelup379452850@gmail.com>
2024-12-24 22:35:28 +0700

    Updated at 202412242235

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -49,7 +49,7 @@ export default async function Page({ searchParams }) {
       </section>
 
       <Suspense fallback={<TransactionListFallback />}>
-        <TransactionList />
+        <TransactionList range={range} />
       </Suspense>
     </>
   );
