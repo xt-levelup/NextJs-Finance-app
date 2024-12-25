b0f8eb08b6130a1555ac5b331184b606ebd36901
Phan Minh Cuong <levelup379452850@gmail.com>
2024-12-25 22:10:30 +0700

    Updated at 202412252210

--- app/(auth)/login/components/login-form.js
+++ app/(auth)/login/components/login-form.js
@@ -2,10 +2,18 @@
 import Input from "@/components/input";
 import { login } from "@/lib/actions";
 import SubmitButton from "@/components/submit-button";
+import { useFormState } from "react-dom";
+
+const initialState = {
+  message: "",
+  error: false,
+};
 
 export default function LoginForm() {
+  const [state, formAction] = useFormState(login, initialState);
+
   return (
-    <form action={login} className="space-y-2">
+    <form action={formAction} className="space-y-2">
       <Input
         type="email"
         placeholder="name@example.com"
@@ -15,6 +23,13 @@ export default function LoginForm() {
       <SubmitButton type="submit" size="sm" className="w-full">
         Sign in with email
       </SubmitButton>
+      <p
+        className={`${
+          state?.error ? "text-red-500" : "text-green-500"
+        } text-sm text-center`}
+      >
+        {state?.message}
+      </p>
     </form>
   );
 }
